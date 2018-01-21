2695176f634a52984ebe55b283bb8fac0d1f323e
Leamare <me@ileamare.ru>
2018-01-22 02:26:49 +0300

    stratz workarounds, moved some functions to modules

--- settings.php
+++ settings.php
@@ -11,10 +11,9 @@ if(!function_exists("readline")) {
 }
 
 # global settings
-  $lrg_version = array(1, 1, 0, -4, 3); # 1.0.0-{alpha/beta/rc1/rc2/release}-rN
+  $lrg_version = array(1, 1, 0, -3, 3); # 1.0.0-{alpha/beta/rc1/rc2/release}-rN
 
 # SQL Connection information
-
   $lrg_sql_host = "localhost";
   $lrg_sql_user = "root";
   $lrg_sql_pass = "";
